f2f9efa2d4f4ecf8c9f3caa7fd7d9539298beeb8
andrew <otmosina@gmail.com>
2013-02-10 16:02:15 +0400

    old sociability

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+#access_token=l8Wiz0y0kIkum7Ms31oc id=3396393
+#Сделать рубильник - голоса считаются при шаринге или при открытии окна соц сети - как на 1001 голос
 # Initialize the rails application
 Battle::Application.initialize!
